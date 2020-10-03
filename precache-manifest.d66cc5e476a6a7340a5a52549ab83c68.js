de2d0106af766b7e66cce7e36c7cc69047594477
Robin-shrestha <rsresthas@gmail.com>
2020-10-03 17:00:44 +0545

    Updates

---
+++ precache-manifest.d66cc5e476a6a7340a5a52549ab83c68.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27fa7c53ed4b3b7958d64da7cc546fa9",
+    "url": "/DirectTech-web-store/index.html"
+  },
+  {
+    "revision": "dc5a33b28cd5c4721eea",
+    "url": "/DirectTech-web-store/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8a7b98ea394403554688",
+    "url": "/DirectTech-web-store/static/js/2.9c46e722.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/DirectTech-web-store/static/js/2.9c46e722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc5a33b28cd5c4721eea",
+    "url": "/DirectTech-web-store/static/js/main.c709545f.chunk.js"
+  },
+  {
+    "revision": "d4993b46a51ec30422b0",
+    "url": "/DirectTech-web-store/static/js/runtime-main.73933e9b.js"
+  },
+  {
+    "revision": "db9d42a8adcde559aeb6786129298439",
+    "url": "/DirectTech-web-store/static/media/1.db9d42a8.jpeg"
+  },
+  {
+    "revision": "b6793510d0ef108c7ee185213f0d2ea6",
+    "url": "/DirectTech-web-store/static/media/2.b6793510.jpg"
+  },
+  {
+    "revision": "945ba71e3f96a7d07bc3ff28359a3cec",
+    "url": "/DirectTech-web-store/static/media/3.945ba71e.jpg"
+  },
+  {
+    "revision": "06df1f5f1f212b03f4f5e1a1730fdc12",
+    "url": "/DirectTech-web-store/static/media/chelsea.06df1f5f.jpg"
+  },
+  {
+    "revision": "23b7c57510ee8e4bc1d1bd326ede9fc8",
+    "url": "/DirectTech-web-store/static/media/chelsea1.23b7c575.jpg"
+  },
+  {
+    "revision": "3dbd28e254fa2d80a4503b7d0ef2c78e",
+    "url": "/DirectTech-web-store/static/media/mutd.3dbd28e2.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c409df7319beef08f830bfb624b16464.js"
+  "/DirectTech-web-store/precache-manifest.d66cc5e476a6a7340a5a52549ab83c68.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DirectTech-web-store/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
